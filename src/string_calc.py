fcb2b271b882ea606e348ab62e386fde7176295c
ArchanSureja <archansureja1@gmail.com>
2025-07-03 17:18:09 +0000

    [Add] Passed two number string input test

--- src/string_calc.py
+++ src/string_calc.py
@@ -2,5 +2,9 @@ class StringCalculator:
     def add(input):
         if len(input)==0:
             return 0
-        else:
+        elif len(input)==1:
             return int(input)
+        else:
+            nums = input.split(",")
+            nums = [int(num) for num in nums]
+            return sum(nums)
