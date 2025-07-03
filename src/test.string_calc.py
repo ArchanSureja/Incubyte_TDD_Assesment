180567848b42c8b21310823c1a37b350b2d7ff8b
ArchanSureja <archansureja1@gmail.com>
2025-07-03 17:37:40 +0000

    [Add] Failed test \n as delimeter

--- src/test.string_calc.py
+++ src/test.string_calc.py
@@ -13,6 +13,11 @@ class StringCalculatorTest(unittest.TestCase):
     
     def test_multiple_num_string_input(self):
         self.assertEqual(10,StringCalculator.add("1,2,3,4"))
+    
+    def test_newline_delimeter(self):
+        self.assertEqual(6,StringCalculator.add("1\n2,3"))
+        self.assertEqual(7,StringCalculator.add("5\n2"))
+        self.assertEqual(11,StringCalculator.add("5\n1\n5"))
 if __name__ == "__main__":
     unittest.main()
 
