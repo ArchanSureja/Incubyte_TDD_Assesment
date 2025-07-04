8a9d90eaf142827c7106a6ad99de60beb9c867a9
ArchanSureja <archansureja1@gmail.com>
2025-07-04 16:35:41 +0000

    [Add] Passed custom delimeter syntax test

--- src/string_calc.py
+++ src/string_calc.py
@@ -3,6 +3,12 @@ class StringCalculator:
         if not input:
             return 0
         else:
+            # custom delimeter
+            if input.startswith("//"):
+                custom_delimeter , input = input.split("\n",1)
+                custom_delimeter=custom_delimeter[2] # skipping first two /
+                input = input.replace(custom_delimeter,",")
+
             input = input.replace("\n",",")
             nums = input.split(",")
             nums = [int(num) for num in nums]
